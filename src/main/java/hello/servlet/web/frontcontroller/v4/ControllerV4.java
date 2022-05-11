98cf9ba5ab557312f484b673e9c3fd6b12e545e0
SEO-my <muyear.seo@gmail.com>
2022-05-11 18:13:03 +0900

    controller V4 Simple

--- src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/ControllerV4.java
@@ -4,5 +4,10 @@ import java.util.Map;
 
 public interface ControllerV4 {
 
+	/**
+	 * @param paramMap
+	 * @param model
+	 * @return viewName
+	 */
 	String process(Map<String, String> paramMap, Map<String, Object> model);
 }
